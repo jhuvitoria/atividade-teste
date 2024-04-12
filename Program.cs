9dde725dc74d9242b0c8518cd73082411cd6af3d
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 19:52:45 -0300

    esta é uma att para aprender na aula.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.WriteLine("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if(numero >= 0) // se o numero for zero ou maior
+{ // faça isso
+Console.ForegroundColor = ConsoleColor.Green; //somente se positivo ou zero
+}
+
+else // senão
+{ // faça isso
+Console.ForegroundColor = ConsoleColor.Red; //somente se negativo   
+}
+
+Console.WriteLine($"O numero digitado é  {numero}."); 
+Console.ResetColor();
